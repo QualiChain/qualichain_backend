c1738a20d347dedf1a537b3acc197b15a7e3a3f7
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-04-15 11:34:58 +0300

    jobs branche merged with master manualy

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ db = SQLAlchemy(app)
 
 api = Api(app)
 
-from models import Book, User, Skill
+from models import Book, User, Skill, Job, UserJob
 
 
 @app.route('/retrieve_data', methods=['POST'])
@@ -282,6 +282,144 @@ class HandleSkill(Resource):
             return ex, 404
 
 
+# =================================
+#   Job APIs
+# =================================
+
+class JobObject(Resource):
+    """ This class is used to retrieve all jobs or add a new job """
+
+    def get(self):
+        """
+        Get All Jobs
+        """
+
+        try:
+            jobs = Job.query.all()
+            serialized_jobs = [job.serialize() for job in jobs]
+            return jsonify(serialized_jobs)
+
+        except Exception as ex:
+            log.error(ex)
+
+    def post(self):
+        """
+        Add new job
+        """
+        data = request.get_json()
+
+        try:
+            job = Job(
+                title=data['title'],
+                job_description=data['jobDescription'],
+                level=data['level'],
+                date=data['date'],
+                start_date=data['startDate'],
+                end_date=data['endDate'],
+                creator_id=data['creatorId'],
+                employment_type=data['employmentType'],
+                skills=data['skills']
+            )
+            db.session.add(job)
+            db.session.commit()
+            return "job added. job={}".format(job.id), 201
+
+        except Exception as ex:
+            log.error(ex)
+            return ex, 400
+
+
+class HandleJob(Resource):
+    """
+    This class is used to get job data using job ID or update job data
+    """
+
+    def get(self, job_id):
+        """
+        Get user
+        """
+        try:
+            job_object = Job.query.filter_by(id=job_id).first()
+            serialized_job = job_object.serialize()
+            return serialized_job
+        except Exception as ex:
+            log.info(ex)
+            return "job with ID: {} does not exist".format(job_id), 404
+
+    def put(self, job_id):
+        """
+        Update user data
+        """
+        data = dict(request.get_json())
+
+        try:
+            job_object = Job.query.filter_by(id=job_id)
+            job_object.update(data)
+            db.session.commit()
+            return "job with ID: {} updated".format(job_id)
+        except Exception as ex:
+            log.error(ex)
+            return ex
+
+
+class UserJobApplication(Resource):
+    """This class is used to create a user-application for a job """
+
+    def post(self, user_id, job_id):
+        """
+        Create user-job relationship
+        """
+        data = request.get_json()
+
+        try:
+            user_job = UserJob(
+                user_id=user_id,
+                job_id=job_id,
+                role=data['role'],
+                available=data['available'],
+                exp_salary=data['expsalary'],
+                score=data['score']
+            )
+            db.session.add(user_job)
+
+            db.session.commit()
+            return "relationship for user={} and job={} created".format(user_id, job_id), 201
+
+        except Exception as ex:
+            log.error(ex)
+            return ex
+
+    def delete(self, user_id, job_id):
+        """
+        delete job application for user and job
+        """
+        try:
+            UserJob.query.filter_by(user_id=user_id, job_id=job_id).delete()
+            db.session.commit()
+            return "job application for job with ID: {} and user with ID: {} deleted".format(job_id, user_id)
+        except Exception as ex:
+            log.error(ex)
+            return ex
+
+
+class JobApplication(Resource):
+    """This class is used to retrieve all applicants for a job """
+
+    def get(self, job_id):
+        """
+        retrieve all applicants
+        """
+        try:
+            print(job_id)
+            applicants = UserJob.query.filter_by(job_id=job_id)
+            print(applicants)
+            serialized_applicants = [applicant.serialize() for applicant in applicants]
+            return serialized_applicants, 200
+        except Exception as ex:
+            log.error(ex)
+            return ex
+
+
 api.add_resource(UserObject, '/users')
 api.add_resource(HandleUser, '/users/<user_id>')
 
@@ -294,3 +432,10 @@ api.add_resource(Authentication, '/auth')
 # Skill Routes
 api.add_resource(SkillObject, '/skills')
 api.add_resource(HandleSkill, '/skills/<skill_id>')
+
+# Job Routes
+api.add_resource(JobObject, '/jobs')
+api.add_resource(HandleJob, '/jobs/<job_id>')
+
+api.add_resource(UserJobApplication, '/jobs/<job_id>/apply/<user_id>')
+api.add_resource(JobApplication, '/jobs/<job_id>/apply/')
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import datetime
 
 import jwt
+from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
@@ -120,4 +121,85 @@ class Skill(db.Model):
         return {
             'id': self.id,
             'name': self.name
+        }
+
+
+class Job(db.Model):
+    __tablename__ = 'jobs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    job_description = db.Column(db.String())
+    level = db.Column(db.String())
+    date = db.Column(db.String())
+    start_date = db.Column(db.String())
+    end_date = db.Column(db.String())
+    creator_id = db.Column(db.Integer())
+    employment_type = db.Column(db.String())
+    skills = db.Column(db.JSON())
+
+    def __init__(self, title, job_description, level, date, start_date, end_date, creator_id, employment_type, skills):
+        self.title = title
+        self.job_description = job_description
+        self.level = level
+        self.date = date
+        self.start_date = start_date
+        self.end_date = end_date
+        self.creator_id = creator_id
+        self.employment_type = employment_type
+        self.skills = skills
+
+    def __repr__(self):
+        return '<id: {} job title: {}>'.format(self.id, self.title)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'job_description': self.job_description,
+            'level': self.level,
+            'date': self.date,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'creator_id': self.creator_id,
+            'employment_type': self.employment_type,
+            'skills': self.skills
+        }
+
+
+class UserJob(db.Model):
+    __tablename__ = 'users_jobs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.ForeignKey(User.id))
+    job_id = db.Column(db.ForeignKey(Job.id))
+    role = db.Column(db.String())
+    available = db.Column(db.String())
+    exp_salary = db.Column(db.String())
+    score = db.Column(db.Integer)
+
+    user = relationship('User', foreign_keys='UserJob.user_id')
+    job = relationship('Job', foreign_keys='UserJob.job_id')
+
+    def __repr__(self):
+        return '<user_id: {} job_id: {}>'.format(self.user_id, self.job_id)
+
+    def __init__(self, user_id, job_id, role, available, exp_salary, score):
+        self.user_id = user_id
+        self.job_id = job_id
+        self.role = role
+        self.available = available
+        self.exp_salary = exp_salary
+        self.score = score
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'job': self.job.serialize(),
+            'user': self.user.serialize(),
+            'role': self.role,
+            'available': self.available,
+            'exp_salary': self.exp_salary,
+            'score': self.score
         }
