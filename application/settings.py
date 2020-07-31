7c818c392ca4f7cba2ba83c1d8302868d1499335
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-07-31 17:15:53 +0300

    Some changes on settings

--- application/settings.py
+++ application/settings.py
@@ -1,33 +1,27 @@
 import os
 
-# APP SETTINGS
 from datetime import datetime
-
-MY_ENV_VAR = os.environ.get('MY_ENV_VAR', 'this_variable')
-API_PORT = os.environ.get('API_PORT', 5000)
-
-FUSEKI_CLIENT_HOST = os.environ.get('FUSEKI_CLIENT_HOST', 'qualichain.epu.ntua.gr')
-FUSEKI_CLIENT_PORT = os.environ.get('FUSEKI_CLIENT_PORT', 3030)
-FUSEKI_CLIENT_DATASET = os.environ.get('FUSEKI_CLIENT_DATASET', 'saro')
-
-RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'qualichain.epu.ntua.gr')
-RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT', 5672)
-RABBITMQ_MNG_PORT = os.environ.get('RABBITMQ_MNG_PORT', 15672)
-RABBITMQ_VHOST = os.environ.get('RABBITMQ_VHOST', '/')
-RABBITMQ_USER = os.environ.get('RABBITMQ_USER', 'rabbitmq')
-RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'rabbitmq')
-RABBITMQ_BEAT_VHOST = os.environ.get('RABBITMQ_BEAT_VHOST', 'backend')
 
 # =================================
 #   APPLICATION SETTINGS
 # =================================
+API_PORT = os.environ.get('API_PORT', 5000)
 APP_QUEUE = os.environ.get('APP_QUEUE', "mediator_queue")
 BEAT_INTERVAL = int(os.getenv('BEAT_INTERVAL', default=60))
-ACTIVE_USER_PERIOD = 2 # DAYS
+ACTIVE_USER_PERIOD = 2  # DAYS
 UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', '/opt/mediator_api/uploads')
 APP_ROOT_PATH = "/opt/mediator_api"
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+APP_SETTINGS = os.environ.get("APP_SETTINGS", "config.DevelopmentConfig")
+SECRET_KEY = os.environ.get("SECRET_KEY", "SECRET_KEY")
+CURRENT_TIME = datetime.now()
+STR_CURRENT_TIME = CURRENT_TIME.strftime("%b %d %Y, %H:%M:%S")
+TOKEN_EXPIRATION = 30  # MINUTES
+
+# =================================
+#   POSTGRES SETTINGS
+# =================================
 DATABASE_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
 DATABASE_USER = os.environ.get('POSTGRES_USER', 'admin')
 DATABASE_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'admin')
@@ -40,22 +34,6 @@ ENGINE_STRING = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
     DATABASE
 )
 
-APP_SETTINGS = os.environ.get("APP_SETTINGS", "config.DevelopmentConfig")
-SECRET_KEY = os.environ.get("SECRET_KEY", "SECRET_KEY")
-CURRENT_TIME = datetime.now()
-STR_CURRENT_TIME = CURRENT_TIME.strftime("%b %d %Y, %H:%M:%S")
-TOKEN_EXPIRATION = 30  # MINUTES
-
-# =================================
-#   EMAIL SERVER SETTINGS
-# =================================
-MAIL_SERVER = os.environ.get('smtp.gmail.com', 'MAIL_SERVER')
-MAIL_PORT = os.environ.get('465', 'MAIL_PORT')
-MAIL_USERNAME = os.environ.get('qualichain@gmail.com', 'MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('qualichain123#', 'MAIL_PASSWORD')
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
-
 # =================================
 #   CURRICULUM DB SETTINGS
 # =================================
@@ -66,6 +44,34 @@ CURRICULUM_DB = {
     "DATABASE": 'api_db'
 }
 CURRICULUM_DB_ENGINE = "postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}/{DATABASE}".format(**CURRICULUM_DB)
+
+# =================================
+#   FUSEKI SETTINGS
+# =================================
+FUSEKI_CLIENT_HOST = os.environ.get('FUSEKI_CLIENT_HOST', 'qualichain.epu.ntua.gr')
+FUSEKI_CLIENT_PORT = os.environ.get('FUSEKI_CLIENT_PORT', 3030)
+FUSEKI_CLIENT_DATASET = os.environ.get('FUSEKI_CLIENT_DATASET', 'saro')
+
+# =================================
+#   RABBITMQ SETTINGS
+# =================================
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'qualichain.epu.ntua.gr')
+RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT', 5672)
+RABBITMQ_MNG_PORT = os.environ.get('RABBITMQ_MNG_PORT', 15672)
+RABBITMQ_VHOST = os.environ.get('RABBITMQ_VHOST', '/')
+RABBITMQ_USER = os.environ.get('RABBITMQ_USER', 'rabbitmq')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'rabbitmq')
+RABBITMQ_BEAT_VHOST = os.environ.get('RABBITMQ_BEAT_VHOST', 'backend')
+
+# =================================
+#   EMAIL SERVER SETTINGS
+# =================================
+MAIL_SERVER = os.environ.get('smtp.gmail.com', 'MAIL_SERVER')
+MAIL_PORT = os.environ.get('465', 'MAIL_PORT')
+MAIL_USERNAME = os.environ.get('qualichain@gmail.com', 'MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('qualichain123#', 'MAIL_PASSWORD')
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
 
 # =================================
 #   UNIVERSAL API SETTINGS
@@ -111,7 +117,6 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 CELERY_TASK_ACKS_LATE = True
 CELERY_TASK_ALWAYS_EAGER = True
-
 
 # =================================
 #   CELERY BEAT SETTINGS
