0c4825781f24dd84a4fe6fa7746094544274f986
epu_temp_steve <sskalidakis@epu.ntua.gr>
2021-04-06 19:48:56 +0300

    fixed settings

--- application/settings.py
+++ application/settings.py
@@ -28,11 +28,11 @@ IAM_PASSWORD = 'iam_f563b972ad'
 # =================================
 #   POSTGRES SETTINGS
 # =================================
-DATABASE_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
-DATABASE_USER = os.environ.get('POSTGRES_USER', 'postgres')
-DATABASE_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'sssshmmy')
+DATABASE_HOST = os.environ.get('POSTGRES_HOST', 'qualichain.epu.ntua.gr')
+DATABASE_USER = os.environ.get('POSTGRES_USER', 'admin')
+DATABASE_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'admin')
 DATABASE = os.environ.get('POSTGRES_DB', 'qualichain_db')
-DATABASE_PORT = os.environ.get('POSTGRESS_PORT', 5432)
+DATABASE_PORT = os.environ.get('POSTGRESS_PORT', 5435)
 
 
 ENGINE_STRING = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(
