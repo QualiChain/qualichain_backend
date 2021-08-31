d18ad5c139c4e785fe11e7aa044e5c8b1c662476
sskalidakis <sskalidakis@epu.ntua.gr>
2021-08-31 13:42:36 +0300

    course recommendation kpi measurement

--- application/course_recommendations/routes.py
+++ application/course_recommendations/routes.py
@@ -7,6 +7,9 @@ from flask_restful import Resource, Api
 from application.course_recommendations import course_recommendation_blueprint
 from application.decorators import only_authenticated
 import json
+from datetime import datetime
+from application.utils import kpi_measurement, kpi_time_measurement
+
 
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
@@ -23,7 +26,12 @@ class SkillCourseRecommendation(Resource):
 
     def post(self):
         request_body = request.json
+        s_time = datetime.now()
         response = requests.post(COURSE_REC_ENDPOINT, json=request_body)
+        kpi_measurement('course_recommendation')
+        e_time = datetime.now()
+        execution_time = (e_time - s_time).microseconds
+        kpi_time_measurement('course_recommendation', execution_time)
         return json.loads(response.content), response.status_code
 
 
