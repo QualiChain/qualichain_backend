45d4b864e4deefb5f614152ab9174a9c2d3b9149
sskalidakis <sskalidakis@epu.ntua.gr>
2021-09-06 16:44:06 +0300

    added pptx and xlsx extensions to file-upload

--- application/settings.py
+++ application/settings.py
@@ -11,7 +11,7 @@ BEAT_INTERVAL = int(os.getenv('BEAT_INTERVAL', default=60))
 ACTIVE_USER_PERIOD = 2  # DAYS
 UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', '/opt/qualichain_backend/uploads')
 APP_ROOT_PATH = "/opt/qualichain_backend"
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'docx', 'xls', 'ppt'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'docx', 'xls', 'ppt', 'xlsx', 'pptx'}
 
 APP_SETTINGS = os.environ.get("APP_SETTINGS", "config.DevelopmentConfig")
 SECRET_KEY = os.environ.get("SECRET_KEY", "SECRET_KEY")
